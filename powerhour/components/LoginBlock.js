8c49e013ad3d00a1a10e1c29dd2e83334959c54d
Dmytri Kislitsyn <dimakislitsyn1996@gmail.com>
2023-05-08 19:17:36 +0300

    Improved design and css, added some minimal functionallity

--- powerhour/components/LoginBlock.js
+++ powerhour/components/LoginBlock.js
@@ -1,25 +1,47 @@
 import Style from '../styles/LoginBlock.module.css';
 import { useRef } from 'react';
 
-function handleClick(labelClass) {
-    const label = document.getElementsByClassName(labelClass)[0];
-    console.log("clicked");
-    console.log(label);
-    label.style.animation = "";
+function handleClickUsername(labelClass) {
+    focusedUsername = true;
 }
+
+function handleClickPassword(labelClass) {
+    focusedPassword = true;
+}
+
+function handleLogin(loginButton) {
+    console.log("test");
+    document.getElementsByClassName(loginButton)[0].style.display = "none";
+    document.getElementsByClassName(`${Style.loadingDiv}`)[0].style.visibility = "visible";
+}
+
+let focusedUsername = false;
+let focusedPassword = false;
+
 
 
 
 export default function LoginBlock() {
-    const labelRef = useRef(null);
+    const usernameRef = useRef(null);
+    const passwordRef = useRef(null);
+
+
     return <>
-    <div className={Style.loginBlock}>
-          <label ref={labelRef} className={Style.loginLabel}>LOGIN</label>
-          <button onClick={() => handleClick(Style.loginLabel)}>ff</button>
-          <label className={Style.usernameLabel}>USERNAME</label>
-          <input type="text" className={Style.username} onClick={() => handleClick(Style.usernameLabel)} placeholder=""></input>
-          <label className={Style.passwordLabel}>PASSWORD</label>
-          <input type="password" className={Style.password} onClick={() => handleClick(Style.passwordLabel)} placeholder=""></input>
+        <div className={Style.loginBlock}>
+            <label className={Style.loginLabel}>LOGIN</label>
+
+            <label className={Style.usernameLabel}>USERNAME</label>
+            <input ref={usernameRef} type="text" className={Style.username} onClick={handleClickUsername} placeholder=""></input>
+            <label className={Style.passwordLabel}>PASSWORD</label>
+            <input ref={passwordRef} type="password" className={Style.password} onClick={handleClickPassword} placeholder=""></input>
+            <div className={Style.buttonDiv}>
+                <button className={Style.loginButton}
+                    onClick={() => handleLogin(Style.loginButton)}>Login</button>
+                <div className={Style.loadingDiv}>
+                    <ion-icon name="refresh-outline" size="large"></ion-icon>
+                </div>
+            </div>
+
         </div>
     </>
 }
