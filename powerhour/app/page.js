5b5ed620a4aebe96864222796f579aa8c809bcbc
Dmytri Kislitsyn <dimakislitsyn1996@gmail.com>
2023-05-15 15:42:43 +0300

    Small tweaks

--- powerhour/app/page.js
+++ powerhour/app/page.js
@@ -47,7 +47,7 @@ export default function Home() {
           <AiFillCaretUp/>
         </div>
         <label className={Style.homeText} onClick={() => movePage(homepage = true)}>HOME</label>
-        <LoginBlock />
+        <LoginBlock /> 
       </div>
 
     </div>
