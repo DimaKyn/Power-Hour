a1868d17dc66a99320c0c7caec635b721047ccb6
Maxim student <lebedinsky.maxim@gmail.com>
2023-05-22 10:56:58 +0300

    Now using bcrypt to encrypt/decrypt passwords. When user registers, his password is hashed. When user login, his password compared using the bcrypt function.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,46 +1,66 @@
+import axios from 'axios';
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import { findUser } from '/lib/findUser';
+import { findUser } from '../../../lib/mongodb';
+import bcrypt from 'bcrypt';
 
-const options = {
-    providers: [
-        CredentialsProvider({
-          async authorize(credentials) {
-            const { username, password } = credentials;
-            const user = await findUser(username, password);
-            
-            if (user) {
-              return Promise.resolve(user);
-            } else {
-              return Promise.resolve(null);
-            }
+async function refreshAccessToken(tokenObject) {
+  try {
+    // Get a new set of tokens with a refreshToken
+    const tokenResponse = await axios.post(YOUR_API_URL + 'auth/refreshToken', {
+      token: tokenObject.refreshToken
+    });
+    return {
+      ...tokenObject,
+      accessToken: tokenResponse.data.accessToken,
+      accessTokenExpiry: tokenResponse.data.accessTokenExpiry,
+      refreshToken: tokenResponse.data.refreshToken
+    }
+  } catch (error) {
+    return {
+      ...tokenObject,
+      error: "RefreshAccessTokenError",
+    }
+  }
+}
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const { username, password } = credentials;
+        const user = await findUser(username);
+    
+        if (user && (await bcrypt.compare(password, user.password))) {
+          return { email: user.email, name: user.name, username: user.username };
+        } else {
+          throw new Error('Invalid credentials');
+        }
       },
     }),
   ],
-  session: {
-    jwt: true,
+  callbacks: {
+    async jwt(token, user, account, profile) {
+      if (user) {
+        token.accessToken = user.data.accessToken;
+        token.accessTokenExpiry = user.data.accessTokenExpiry;
+        token.refreshToken = user.data.refreshToken;
+      }
+      const shouldRefreshTime = Math.round((token.accessTokenExpiry - 60 * 60 * 1000) - Date.now());
+      if (shouldRefreshTime > 0) {
+        return token;
+      }
+      return refreshAccessToken(token);
+    },
+    async session(session, token) {
+      session.accessToken = token.accessToken;
+      session.accessTokenExpiry = token.accessTokenExpiry;
+      session.error = token.error;
+      return session;
+    },
   },
-};
-
-export default (req, res) => NextAuth(req, res, options);
-
-
-
-/** Example of how to hide content from users that did not logged in
- * import { useSession } from 'next-auth/client';
-
-function ProtectedComponent() {
-  const [session, loading] = useSession();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!session) {
-    return <div>Access Denied</div>;
-  }
-
-  return <div>Protected content goes here</div>;
-}
-
- */
+  events: {
+    // Disable broadcasting the getSession event
+    getSession: false,
+  },
+});
--- pages/api/login.js
+++ pages/api/login.js
@@ -1,14 +1,20 @@
-// pages/api/login.js
+import bcrypt from 'bcrypt';
 import { findUser } from '../../lib/mongodb';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { username, password } = req.body;
     try {
-      const user = await findUser(username, password);
-      
+      const user = await findUser(username);
+
+      // If the user exists, compare the entered password with the stored hashed password
       if (user) {
-        res.status(200).json({ success: true, user });
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (isPasswordValid) {
+          res.status(200).json({ success: true, user });
+        } else {
+          res.status(401).json({ success: false, message: 'Invalid credentials' });
+        }
       } else {
         res.status(401).json({ success: false, message: 'Invalid credentials' });
       }
