710e859643c4e68bd290649e4ad13389ec029cd0
Dmytri Kislitsyn <dimakislitsyn1996@gmail.com>
2023-06-02 11:47:50 +0300

    cool commit

--- components/workoutsBlock/WorkoutBox.jsx
+++ components/workoutsBlock/WorkoutBox.jsx
@@ -15,9 +15,9 @@ export default function WorkoutBox(props) {
   return <div className={containerStatus}>
         <BsArrowsMove className={Style.moveSquareIcon} />
         <h3 style={{ fontSize: "20px" }}>{title}</h3>
-        <Image style={{ pointerEvents: "none" }} src={imageSrc} width={imageWidth} height={imageHeight} />
-        <label style={{ pointerEvents: "none" }} >{reps}</label>
-        <label style={{ pointerEvents: "none" }} >{weight}</label>
+        <Image style={{pointerEvents: "none"}} src={imageSrc} width={imageWidth} height={imageHeight} />
+        <label style={{pointerEvents: "none"}} >{reps}</label>
+        <label style={{pointerEvents: "none"}} >{weight}</label>
         <div style={{ left: "0", bottom: "0", width: "100%", height: "100%" }}>
           <button className={Style.greenCheckMark}><GrCheckmark className={Style.checkmark} /></button>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import Style from '/styles/Home.module.css';
 import LoginBlock from '../components/LoginBlock';
-import { AiFillCaretDown, AiFillCaretUp} from 'react-icons/ai';
+import { AiFillCaretDown, AiFillCaretUp } from 'react-icons/ai';
 //Import the navigation panel for this specific page
 import NavigationPanel from '/components/navigationPanel/NavigationPanel';
 import { homePanelLinks } from '/components/navigationPanel/NavigationPanelLinksList';
 
 //On a click of a label, move the page to the login block or to the top of the page
 function movePage(homepage) {
-  //labelRef is the current clicked on element
   homepage ? window.scrollTo({ top: 0, behavior: 'smooth' }) :
     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
   homepage = !homepage;
@@ -20,7 +19,7 @@ export default function Home() {
   let homepage = true;
   return (
     <div className={Style.main}>
-      <NavigationPanel links={homePanelLinks}/>
+      <NavigationPanel links={homePanelLinks} />
       <div className={Style.wrapper}>
         <div className={Style.backgroundImage}></div>
         <div className={Style.homepageLabels}>
@@ -29,7 +28,7 @@ export default function Home() {
           <div className={Style.subSubSubImage}></div>
           <label className={Style.loginText} onClick={() => movePage(homepage = false)}>LOGIN TO START TRAINING</label>
           <div className={Style.arrowDivDown} onClick={() => movePage(homepage = false)}>
-            <AiFillCaretDown/>
+            <AiFillCaretDown />
           </div>
         </div>
       </div>
@@ -37,10 +36,10 @@ export default function Home() {
       <div className={Style.loginPage}>
         <div className={Style.backgroundImage2}></div>
         <div className={Style.arrowDivUp} onClick={() => movePage(homepage = true)}>
-          <AiFillCaretUp/>
+          <AiFillCaretUp />
         </div>
         <label className={Style.homeText} onClick={() => movePage(homepage = true)}>HOME</label>
-        <LoginBlock /> 
+        <LoginBlock />
       </div>
     </div>
   )
