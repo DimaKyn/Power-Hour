8d4dad10c10793330ec5f69c0aa10b21a3cbcd1d
Maxim student <lebedinsky.maxim@gmail.com>
2023-06-05 22:31:59 +0300

    added save buttom (no logic yet)

--- components/WorkoutContainer.jsx
+++ components/WorkoutContainer.jsx
@@ -2,47 +2,71 @@ import React, { useState } from 'react';
 import Style from '/styles/WorkoutContainer.module.css';
 
 const Exercise = ({ exercise }) => {
-  const [sets, setSets] = useState(exercise.sets);
-  const [reps, setReps] = useState(exercise.reps);
+  const [setsReps, setSetsReps] = useState({ sets: exercise.sets, reps: exercise.reps });
   const [editing, setEditing] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
 
-  const handleSetsChange = (event) => {
-    setSets(event.target.value);
-  };
 
-  const handleRepsChange = (event) => {
-    setReps(event.target.value);
+  const handleSetsRepsChange = (event) => {
+    const value = parseInt(event.target.value);
+    const name = event.target.name;
+    if (!isNaN(value) && value >= 0) {
+      setSetsReps(prevState => ({ ...prevState, [name]: value }));
+    }
   };
 
   const handleEditClick = () => {
     setEditing(!editing);
   };
 
+  const handleSaveClick = () => {
+    setIsSaving(true);
+  };
+  
+
   return (
     <div>
-      <h3 className={Style.h3}>{exercise.name}</h3>
-      {editing ? (
+    <h3 className={Style.h3}>{exercise.name}</h3>
+    {editing ? (
+      <div>
         <div>
-          <div>
-            <label>Sets:</label>
-            <input type="number" value={sets} onChange={handleSetsChange} />
-          </div>
-          <div>
-            <label>Reps:</label>
-            <input type="number" value={reps} onChange={handleRepsChange} />
-          </div>
-          <button onClick={handleEditClick}>Save</button>
+          <label>Sets:</label>
+          <input
+            type="number"
+            name="sets"
+            value={setsReps.sets}
+            onChange={handleSetsRepsChange}
+            className={Style.inputBlack}
+          />
         </div>
-      ) : (
         <div>
-          <p className={Style.p}>
-            {exercise.sets} {exercise.reps}
-          </p>
-          <button onClick={handleEditClick}>Edit</button>
+          <label>Reps:</label>
+          <input
+            type="number"
+            name="reps"
+            value={setsReps.reps}
+            onChange={handleSetsRepsChange}
+            className={Style.inputBlack}
+          />
         </div>
-      )}
-      <img src={exercise.imageUrl} alt={exercise.name} />
-    </div>
+        {isSaving ? (
+          <button className={Style.button}>Saving...</button>
+        ) : (
+          <button className={Style.button} onClick={handleSaveClick}>
+            Save
+          </button>
+        )}
+      </div>
+    ) : (
+      <div>
+        <p className={Style.p}>
+          {exercise.sets} {exercise.reps}
+        </p>
+        <button onClick={handleEditClick}>Edit</button>
+      </div>
+    )}
+    <img src={exercise.imageUrl} alt={exercise.name} />
+  </div>
   );
 };
 
@@ -52,15 +76,18 @@ const WorkoutContainer = ({ workoutName, category }) => {
 
   return (
     <div className={Style.container}>
-      <h2 className={Style.h2}>{workoutName}</h2>
-      <div className={Style.exercisesContainer}>
-        {exercises.map((exercise) => (
-          <div key={exercise.id}>
-            <Exercise exercise={exercise} />
-          </div>
-        ))}
-      </div>
+    <h2 className={Style.h2}>{workoutName}</h2>
+    <div className={Style.exercisesContainer}>
+      {exercises.map((exercise) => (
+        <div key={exercise.id}>
+          <Exercise exercise={exercise} />
+        </div>
+      ))}
     </div>
+    <div className={Style.buttonContainer}>
+      <button className={Style.button}>Save</button>
+    </div>
+  </div>
   );
 };
 
