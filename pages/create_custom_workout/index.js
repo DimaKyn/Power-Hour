0d1056d16f1b44883db20cf2d9f294edd6a8635d
Dmytri Kislitsyn <dimakislitsyn1996@gmail.com>
2023-06-15 11:18:39 +0300

    Don't remember what changed

--- pages/create_custom_workout/index.js
+++ pages/create_custom_workout/index.js
@@ -5,14 +5,24 @@ import SearchBox from '/components/searchBox/SearchBox';
 
 
 
-export default function CreateCustomWorkout() { 
+
+export default function CreateCustomWorkout() {
 
     return <>
-         <NavigationPanel links={createCustomWorkoutPanelLinks} />
+        <NavigationPanel links={createCustomWorkoutPanelLinks} />
         <div className={Style.inner}>
-            <label className={Style.mainLabel}>Create Custom</label>
-            <SearchBox className={Style.searchBox}></SearchBox>
-            <div className={Style.workouts}></div>
+            <script>
+                {window.onbeforeunload = () => {
+                    return true;
+                }}
+            </script>
+            <label className={Style.mainLabel}>Create Workout</label>
+            <div className={Style.innerRow}>
+                <SearchBox className={Style.searchBox}></SearchBox>
+                <div className={Style.workouts}></div>
+            </div>
         </div>
+
+
     </>
 }
