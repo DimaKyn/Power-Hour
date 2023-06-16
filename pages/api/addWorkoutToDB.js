406ba486952e7269137d71b706cc059889138dbf
Maxim Lebedinsky <lebedinsky.maxim@gmail.com>
2023-06-16 19:18:57 +0300

    Adding workouts from user to database

--- pages/api/addWorkoutToDB.js
+++ pages/api/addWorkoutToDB.js
@@ -1,34 +1,32 @@
 import clientPromise from '/lib/mongodb';
-import { getSession } from 'next-auth/react';
+import { getServerSession } from "next-auth/next"
 
 export default async function handler(req, res) {
-    //Get session and to extract username from that
-    try {
-        const session = await getSession();
-        if (!session) {
-            // Handle unauthorized access
-            return res.status(401).json({ message: 'Unauthorized' });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Error getting session", error: error.message });
-    }
-
+    
     if (req.method !== 'POST') {
         return res.status(405).json({ msg: 'Method not allowed' });
     }
 
-    const workout = req.body;
-
     try {
+        const session = await getServerSession(req, res)
+        console.log(session);
+        if (!session) {
+            // Handle unauthorized access
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const workout = req.body;
+        console.log(session.user)
+
         const client = await clientPromise;
         const db = client.db("powerhourdb");
         const workoutsCollection = db.collection("UserCustomWorkouts");
         const result = await workoutsCollection.findOneAndUpdate(
-            { username: session.user.username },
+            { email: session.user.email },
             { $push: { workoutsArray: workout } },
             { returnOriginal: false, upsert: true }
         );
-        res.status(200).json({ message: "Workout added successfully", insertedId: result.insertedId })
+        res.status(200).json({ message: "Workout added successfully", insertedId: result.insertedId });
     } catch (error) {
         res.status(500).json({ message: "Error adding workout", error: error.message });
     }
