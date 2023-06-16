027c3f2f35fb4fdf192f2d4b4d7011e75bbffd62
Maxim Lebedinsky <lebedinsky.maxim@gmail.com>
2023-06-16 20:11:13 +0300

    Display the workout name and exercises

--- pages/custom_workout/index.js
+++ pages/custom_workout/index.js
@@ -14,7 +14,9 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export default function CustomWorkout() {
     const router = useRouter();
-    const { workout } = router.query;
+    const { workout, exercise } = router.query;
+    const parsedExercise = JSON.parse(exercise);
+    console.log(parsedExercise)
 
     // size may also be a plain string using the presets 'large' or 'compact'
     const size = {
@@ -37,17 +39,19 @@ export default function CustomWorkout() {
                     breakpoints={{ lg: 1500, md: 1000, sm: 750, xs: 500, xxs: 250 }}
                     cols={{ lg: 5, md: 4, sm: 3, xs: 2, xxs: 1 }}
                 >
-                    <div key="d" data-grid={{ x: 0, y: 0, w: 1, h: 2, minW: 1, maxW: 1 }} className={Style.gridElement}>
-                    </div>
-                    <div key="a" data-grid={{ x: 0, y: 1, w: 1, h: 2, minW: 1, maxW: 1 }} className={Style.gridElement}>
-                        <WorkoutBox title={""} reps={""} weight={""} imageSrc={"/lift.jpg"} imageHeight={"150"} imageWidth={"150"} containerName={"workoutBox"} />
-                    </div>
-                    <div key="b" data-grid={{ x: 1, y: 0, w: 1, h: 2, minW: 1, maxW: 1 }} className={Style.gridElement}>
-                        <WorkoutBox title={""} reps={""} weight={""} imageSrc={"/chalk.jpg"} imageHeight={"150"} imageWidth={"150"} />
-                    </div>
-                    <div key="c" data-grid={{ x: 1, y: 1, w: 1, h: 2, minW: 1, maxW: 1 }} className={Style.gridElement}>
-                        <WorkoutBox title={""} reps={""} weight={""} imageSrc={"/liftingWeights.jpg"} imageHeight={"150"} imageWidth={"150"} />
-                    </div>
+                    {parsedExercise.map((exercise, index) => (
+                        <div key={index.toString()} data-grid={{ x: 0, y: 0, w: 1, h: 2, minW: 1, maxW: 1 }} className={Style.gridElement}>
+                            <WorkoutBox
+                                title={exercise.name}
+                                reps={exercise.reps}
+                                //weight={exercise.weight}
+                                // imageSrc={exercise.imageSrc}
+                                // imageHeight={exercise.imageHeight}
+                                // imageWidth={exercise.imageWidth}
+                                containerName={"workoutBox"}
+                            />
+                        </div>
+                    ))}
                 </ResponsiveGridLayout>
                 <StopWatch />
             </div>
