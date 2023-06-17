60248037735951da75554fc38063821855b6012e
Dmytri Kislitsyn <dimakislitsyn1996@gmail.com>
2023-06-18 01:19:58 +0300

    Fit entire website for phones, needs little tweaks

--- components/ProfileActivities.jsx
+++ components/ProfileActivities.jsx
@@ -1,4 +1,5 @@
 import Style from "/styles/ProfileActivities.module.css";
+import StyleStandard from "/styles/PageStandard.module.css";
 import Link from "next/link";
 import { GiBiceps } from "react-icons/gi";
 import { BsPlusLg, BsGraphUpArrow } from "react-icons/bs";
@@ -7,26 +8,27 @@ import { VscDebugStart } from "react-icons/vsc";
 
 
 export default function ProfileActivities() {
-    return <div className={Style.profileContainer}>
+    return <>
         <div className={Style.profilePicture}></div>
         <div className={Style.buttonContainer}>
+
             <div className={Style.choiceBlock}>
-                <h1 style={{ fontSize: "27px" }}>Check out a workout</h1>
-                <span className={Style.span}>Choose a plan from a dozen of our available workouts.</span>
+                <h1 style={{ fontSize: "27px", textAlign: "center" }}>Check out a workout</h1>
+                <span className={Style.span}>Choose a plan from a few of our available workouts.</span>
                 <Link href="./premade_workout" className={Style.button}>
                     <VscDebugStart style={{ justifyContent: "center", position: "center", paddingRight: "10px", fontSize: "30px" }} />
                     Browse Workouts</Link>
 
             </div>
             <div className={Style.choiceBlock}>
-                <h1 style={{ fontSize: "27px" }}>Feeling creative?</h1>
+                <h1 style={{ fontSize: "27px", textAlign: "center" }}>Feeling creative?</h1>
                 <span className={Style.span}>Create the perfect routine to suit your style, technique, preferences and workout needs. <br />Browse from hundreds of available exercises.</span>
                 <Link href="./create_custom_workout" className={Style.button}>
                     <BsPlusLg style={{ justifyContent: "center", position: "center", paddingRight: "10px", fontSize: "30px" }} />
                     Create Workout</Link>
             </div>
             <div className={Style.choiceBlock}>
-                <h1 style={{ fontSize: "27px", testAlign: "center" }}>Browse custom workouts</h1>
+                <h1 style={{ fontSize: "27px", textAlign: "center" }}>Browse custom workouts</h1>
 
                 <span className={Style.span}>Start a workout that you built for yourself.</span>
                 <Link href="./custom" className={Style.button}>
@@ -34,12 +36,12 @@ export default function ProfileActivities() {
                     Custom Workouts</Link>
             </div>
             <div className={Style.choiceBlock}>
-                <h1 style={{ fontSize: "27px" }}>For you</h1>
+                <h1 style={{ fontSize: "27px", textAlign: "center" }}>For you</h1>
                 <span className={Style.span}>Track your progress, Keep tabs on what's important.</span>
                 <Link href="./personal_details" className={Style.button}>
                     <BsGraphUpArrow style={{ justifyContent: "center", position: "center", paddingRight: "10px", fontSize: "30px" }} />
                     Update Progress</Link>
             </div>
         </div>
-    </div>
+    </>
 }
