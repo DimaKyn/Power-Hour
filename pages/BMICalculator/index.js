6a349ec343494d9f1eb97662cc1ebd9c04426462
Dmytri Kislitsyn <dimakislitsyn1996@gmail.com>
2023-06-17 13:53:43 +0300

    Can now calculate BMI - needs styling

--- pages/BMICalculator/index.js
+++ pages/BMICalculator/index.js
@@ -4,7 +4,6 @@ import Style from "/styles/PageStandard.module.css";
 export default function BMICalculatorPage() {
     return <div className={Style.inner}>
         <BMICalculator />
-
     </div>
     ;
 }
--- pages/api/fetchWorkouts.js
+++ pages/api/fetchWorkouts.js
@@ -2,22 +2,23 @@ import clientPromise from '/lib/mongodb';
 import { getSession } from 'next-auth/react';
 
 export default async function handler(req, res) {
+
     if (req.method !== 'GET') {
         return res.status(405).json({ msg: 'Method not allowed' });
     }
+
   try {
-   
+
     const session = await getSession({ req });
     console.log(session)
     if (!session) {
       // Handle unauthorized access
       return res.status(401).json({ message: 'Unauthorized' });
     }
-
     const client = await clientPromise;
     const db = client.db("powerhourdb");
     const workoutsCollection = db.collection("UserCustomWorkouts");
-    const cursor = await workoutsCollection.find({ email: session.user.email});
+    const cursor = await workoutsCollection.find({ email: session.user.email });
     const result = await cursor.toArray();
     res.status(200).json(result[0]);
   } catch (error) {
