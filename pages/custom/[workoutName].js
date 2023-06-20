5ec9a868251c7b86994c3518893b66b06a3c1248
Dmytri Kislitsyn <dimakislitsyn1996@gmail.com>
2023-06-20 15:47:33 +0300

    Fixed rendering issues for custom exercise boxes

--- pages/custom/[workoutName].js
+++ pages/custom/[workoutName].js
@@ -8,7 +8,7 @@ import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
 import { Responsive, WidthProvider } from 'react-grid-layout';
 import Layouts from 'react-grid-layout'; // using @types/react-grid-layout
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 import { BsArrowsMove } from "react-icons/bs";
@@ -18,18 +18,30 @@ import { BsArrowsMove } from "react-icons/bs";
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export default function CustomWorkout() {
-    const exercise = localStorage.getItem('exercises');
-    const workout = localStorage.getItem('workout')
-    const parsedExercise = JSON.parse(exercise);
+    const [parsedExercise, setParsedExercise] = useState(null);
+    const [workout, setWorkout] = useState(null);
     const [layouts, setLayouts] = useState();
     const handleLayoutChange = useCallback((layout, layouts) => setLayouts(layouts));
+  
+    useEffect(() => {
+      const storedExercise = localStorage.getItem('exercises');
+      const storedWorkout = localStorage.getItem('workout');
+      if (storedExercise) {
+        setParsedExercise(JSON.parse(storedExercise));
+      }
+      if (storedWorkout) {
+        setWorkout(storedWorkout);
+      }
+    }, []);
 
     return <>
         <NavigationPanel links={customWorkoutPanelLinks} />
         <div className={Style.innerScrollable}>
             <label className={Style.mainLabel}>{workout}</label>
             <div className={Style.textWithIconInTheMiddle}>
-                <label>Move the blocks by dragging the &nbsp;</label> <BsArrowsMove />&nbsp; icon
+                <span className={Style.spanTextWithIconInTheMiddle}>Move the blocks by dragging the&nbsp;</span>
+                <BsArrowsMove className={Style.spanTextWithIconInTheMiddle}/>
+                <span className={Style.spanTextWithIconInTheMiddle}>&nbsp;icon</span>
 
             </div>
 
@@ -43,7 +55,8 @@ export default function CustomWorkout() {
                     cols={{ lg: 6, md: 4, sm: 3, xs: 2, xxs: 1 }}
                 >
                     {parsedExercise.map((exercise, index) => (
-                        <div key={index.toString()} data-grid={{ x: 1, y: 1, w: 1, h: 1, minW: 1, maxW: 1, maxH: 1, minH: 1 }} className={Style.gridElement}>
+
+                        <div key={index.toString()} data-grid={{ x: index, y: 0, w: 1, h: 1, minW: 1, maxW: 1, maxH: 1, minH: 1 }} className={Style.gridElement}>
                             <WorkoutBox
                                 title={exercise.name}
                                 sets={exercise.sets}
